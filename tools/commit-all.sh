de8afae3511b163cb157e296bbc6f9d16b12ccb9
Ocawesome101 <ocawesome101@gmail.com>
2022-02-22 16:54:24 -0500

    several fixes / improvements

--- tools/commit-all.sh
+++ tools/commit-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git submodule foreach foreach git add .
-git submodule foreach git commit
+git submodule foreach "git add .; true"
+git submodule foreach "git commit; true"
 
 git add .
 if [ "$#" -lt 0 ] ; then
