96302e25e0fe11b64f225d2dafcca2d162bbfca0
Ocawesome101 <ocawesome101@gmail.com>
2022-11-14 12:14:21 -0500

    all packages should build now

--- tools/push-repos.sh
+++ tools/push-repos.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-push () {
-  cd $1
-  git add .
-  git commit "$@"
-  git push
-  cd ..
-}
-
 git submodule foreach "git add .; true"
-git submodule foreach "git commit; true"
+git submodule foreach "git commit $@; true"
 
 git add .
 git commit "$@"
 
-git submodule foreach "git push; true"
+git submodule foreach "git push $@; true"
 git push
